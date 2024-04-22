8a27172d4249866a9ad6892a6b19c91fd7190a81
Torathion <torathion@gmail.com>
2024-04-23 00:17:45 +0200

    feat: check for restricted window globals

--- src/profiles/web.ts
+++ src/profiles/web.ts
@@ -5,6 +5,67 @@ import sdl from '@microsoft/eslint-plugin-sdl'
 
 import type { PartialProfileConfig } from '../types/interfaces'
 
+const ConfusingBrowserGlobals = [
+    'addEventListener',
+    'blur',
+    'close',
+    'closed',
+    'confirm',
+    'defaultStatus',
+    'defaultstatus',
+    'event',
+    'external',
+    'find',
+    'focus',
+    'frameElement',
+    'frames',
+    'history',
+    'innerHeight',
+    'innerWidth',
+    'length',
+    'location',
+    'locationbar',
+    'menubar',
+    'moveBy',
+    'moveTo',
+    'name',
+    'onblur',
+    'onerror',
+    'onfocus',
+    'onload',
+    'onresize',
+    'onunload',
+    'open',
+    'opener',
+    'opera',
+    'outerHeight',
+    'outerWidth',
+    'pageXOffset',
+    'pageYOffset',
+    'parent',
+    'print',
+    'removeEventListener',
+    'resizeBy',
+    'resizeTo',
+    'screen',
+    'screenLeft',
+    'screenTop',
+    'screenX',
+    'screenY',
+    'scroll',
+    'scrollbars',
+    'scrollBy',
+    'scrollTo',
+    'scrollX',
+    'scrollY',
+    'self',
+    'status',
+    'statusbar',
+    'stop',
+    'toolbar',
+    'top'
+]
+
 export const config: PartialProfileConfig = {
     apply: { compat, 'ssr-friendly': ssr, sdl },
     extends: ['base'],
@@ -20,7 +81,8 @@ export const config: PartialProfileConfig = {
             'sdl/no-insecure-url': 2,
             'sdl/no-msapp-exec-unsafe': 2,
             'sdl/no-postmessage-star-origin': 2,
-            'sdl/no-winjs-html-unsafe': 2
+            'sdl/no-winjs-html-unsafe': 2,
+            'no-restricted-globals': [2, ...ConfusingBrowserGlobals]
         }
     ]
 }
