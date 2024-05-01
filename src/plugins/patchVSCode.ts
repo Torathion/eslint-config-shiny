d9cd0ea68c33c4816cc73f3c34a9ed0e6726252b
Torathion <torathion@gmail.com>
2024-05-01 19:57:34 +0200

    fix: vscode patch file merging

--- src/plugins/patchVSCode.ts
+++ src/plugins/patchVSCode.ts
@@ -2,6 +2,7 @@ import { existsSync } from 'node:fs'
 import { mkdir, open, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import type { ShinyConfig } from 'src/types/interfaces'
+import fileToJson from 'src/utils/fileToJson'
 
 const VSCodePatch = {
     // Auto fix
@@ -47,14 +48,18 @@ export default async function patchVSCode(opts: ShinyConfig): Promise<void> {
     if (!existsSync(vsCodeFolderPath)) await mkdir(vsCodeFolderPath)
     if (!existsSync(settingsPath)) await writeFile(settingsPath, JSON.stringify(VSCodePatch), 'utf8')
     const file = await open(settingsPath, 'r+')
-    const settings = JSON.parse((await file.readFile()).toString())
+    const settings = await fileToJson(file)
     let shouldWrite = true
     for (const key of keys) {
+        // only write when there are no eslint keys inside the settings.json
         if (settings[key]) {
             shouldWrite = false
             break
         }
     }
-    if (shouldWrite) await file.writeFile(JSON.stringify(Object.assign(settings, VSCodePatch)))
+    if (shouldWrite) {
+        const buffer = Buffer.from(JSON.stringify(Object.assign(settings, VSCodePatch)))
+        await file.write(buffer, 0, buffer.byteLength, 0)
+    }
     await file.close()
 }
