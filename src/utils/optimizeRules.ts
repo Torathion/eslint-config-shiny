9436353c03608a2adc37e5d6963857ed868cda71
Torathion <torathion@gmail.com>
2024-10-24 18:32:49 +0200

    change: overhaul rule value optimization

--- src/utils/optimizeRules.ts
+++ src/utils/optimizeRules.ts
@@ -13,9 +13,20 @@ function renameRule(rule: string, renames: Record<string, string>, rename: strin
     return (newString.match(regex)?.length ?? 0) < 2 ? newString : newString.replace('/', '-')
 }
 
+function optimizeRuleValue(entry: SharedConfig.RuleEntry | undefined): SharedConfig.RuleEntry {
+    // if, for some reason, the rule entry is undefined, just turn off the rule
+    if (!entry) return 0
+    if (typeof entry === 'string') return ESLintValueMapper[entry] ?? 0 // if the rule has a weird string as value, just turn it off.
+    if (Array.isArray(entry) && typeof entry[0] === 'string') {
+        entry[0] = ESLintValueMapper[entry[0]]
+        return entry
+    }
+    return entry
+}
+
 export default function optimizeRules(rules: SharedConfig.RulesRecord, renames: Record<string, string>): void {
     for (const rule of Object.keys(rules)) {
-        rules[rule] = typeof rules[rule] === 'string' ? ESLintValueMapper[rules[rule]] : rules[rule] ?? 0
+        rules[rule] = optimizeRuleValue(rules[rule])
         for (const rename of Object.keys(renames)) {
             if (rule.startsWith(rename)) {
                 rules[renameRule(rule, renames, rename)] = rules[rule]
