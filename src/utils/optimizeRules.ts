d8faf533d2ec5f2919c32e106a6502a98f420a4b
Torathion <torathion@gmail.com>
2024-10-24 13:40:14 +0200

    fix: optimizeRules accidentally deleting renamed rules

--- src/utils/optimizeRules.ts
+++ src/utils/optimizeRules.ts
@@ -6,13 +6,11 @@ const ESLintValueMapper: Record<string, SharedConfig.RuleLevel> = {
     error: 2
 }
 
-const regex = /\//
+const regex = /\//g
 
-function renameSubPackageRule(rule: string, renames: Record<string, string>, rename: string): string {
-    const count = regex.exec(rule)?.length ?? 0
-    if (count < 2) return rule
-    // Only replace the first of both / with a -
-    return rule.replace(rename, renames[rename]).replace('/', '-')
+function renameRule(rule: string, renames: Record<string, string>, rename: string): string {
+    const newString = rule.replace(rename, renames[rename])
+    return (newString.match(regex)?.length ?? 0) < 2 ? newString : newString.replace('/', '-')
 }
 
 export default function optimizeRules(rules: SharedConfig.RulesRecord, renames: Record<string, string>): void {
@@ -20,7 +18,7 @@ export default function optimizeRules(rules: SharedConfig.RulesRecord, renames: 
         rules[rule] = typeof rules[rule] === 'string' ? ESLintValueMapper[rules[rule]] : rules[rule] ?? 0
         for (const rename of Object.keys(renames)) {
             if (rule.startsWith(rename)) {
-                rules[renameSubPackageRule(rule, renames, rename)] = rules[rule]
+                rules[renameRule(rule, renames, rename)] = rules[rule]
                 delete rules[rule]
             }
         }
--- test.js
+++ test.js
@@ -1,33 +1,6 @@
-const plugins = {
-    '@eslint-react/debug': 0,
-    '@eslint-react/dom': 1,
-    '@eslint-react/hooks-extra': 2,
-    '@eslint-react/naming-convention': 3,
-    '@eslint-react/web-api': 4
-}
-const renames = {
-    '@arthurgeron/react-usememo': 'use-memo',
-    '@eslint-react': 'react',
-    '@microsoft/sdl': 'sdl',
-    '@stylistic/js': 'styleJs',
-    '@stylistic/jsx': 'styleJsx',
-    '@stylistic/ts': 'styleTs',
-    '@typescript-eslint': 'ts'
+function countSlashes(str) {
+    const matches = str.match(/\//g)
+    return matches ? matches.length : 0
 }
 
-export default function renamePlugins(plugins, renames) {
-    if (!plugins) return plugins
-    for (const name of Object.keys(plugins)) {
-        for (const key of Object.keys(renames)) {
-            if (name.startsWith(key)) {
-                plugins[name.replace(key, renames[key])] = plugins[name]
-                delete plugins[name]
-            }
-        }
-    }
-    return plugins
-}
-
-console.time('renamePlugins')
-renamePlugins(plugins, renames)
-console.timeEnd('renamePlugins')
+console.log(countSlashes('arrow-body-style'))
