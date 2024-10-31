599722090680c77c7c6e9fb6bae89cb3bb16fc92
Torathion <torathion@gmail.com>
2024-10-31 20:52:03 +0100

    change: remove eslint-plugin-package-json for not exposing plugin for this tool

--- src/profiles/json.ts
+++ src/profiles/json.ts
@@ -1,50 +1,20 @@
 import json from 'eslint-plugin-json'
 import jsonFiles from 'eslint-plugin-json-files'
-// import packageJson from 'eslint-plugin-package-json'
-import jsoncParser from 'jsonc-eslint-parser'
 
 import type { PartialProfileConfig } from '../types/interfaces'
 
-export default [
-    {
-        apply: { json },
-        files: ['**/package.json'],
-        name: 'package.json',
-        plugins: {
-            'json-files': jsonFiles
-            //'package-json': packageJson
-        },
-        rules: [
-            {
-                'json-files/ensure-repository-directory': 2,
-                'json-files/require-engines': 2,
-                'json-files/require-license': 2,
-                'json-files/require-unique-dependency-names': 2
-                /*
-                 * 'package-json/order-properties': 1,
-                 * 'package-json/prefer-repository-shorthand': 1,
-                 * 'package-json/sort-collections': 1,
-                 * 'package-json/unique-dependencies': 2,
-                 * 'package-json/valid-local-dependency': 2,
-                 * 'package-json/valid-name': 2,
-                 * 'package-json/valid-package-def': 2,
-                 * 'package-json/valid-repository-directory': 1,
-                 * 'package-json/valid-version': 2
-                 */
-            }
-        ]
+const config: PartialProfileConfig = {
+    apply: { json },
+    files: ['**/*.json'],
+    name: 'json',
+    plugins: {
+        'json-files': jsonFiles
     },
-    {
-        apply: { json },
-        files: ['**/*.json'],
-        name: 'json',
-        plugins: {
-            'json-files': jsonFiles
-        },
-        rules: [
-            {
-                'json-files/validate-schema': 0
-            }
-        ]
-    }
-] as Partial<PartialProfileConfig>[]
+    rules: [
+        {
+            'json-files/validate-schema': 0
+        }
+    ]
+}
+
+export default config
