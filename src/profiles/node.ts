8c1e9f6b9f9465fdc587fad7729604762b092857
Torathion <torathion@gmail.com>
2024-11-05 00:50:45 +0100

    change: remove plugin security node for being abandoned

--- src/profiles/node.ts
+++ src/profiles/node.ts
@@ -1,14 +1,10 @@
 import globals from 'globals'
 import n from 'eslint-plugin-n'
 import sdl from '@microsoft/eslint-plugin-sdl'
-import nodeSecurity from 'eslint-plugin-security-node'
 
 import type { PartialProfileConfig } from '../types/interfaces'
 
 export const config: PartialProfileConfig = {
-    apply: {
-        'security-node': nodeSecurity
-    },
     extends: ['base', n.configs['flat/recommended-module']],
     languageOptions: {
         globals: globals.node
