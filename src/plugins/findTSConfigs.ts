8636d7fb20eda333f3c62f9b585125d80d81ef95
Torathion <torathion@gmail.com>
2024-11-10 23:49:40 +0100

    fix: default project service using a nonsensical file

--- src/plugins/findTSConfigs.ts
+++ src/plugins/findTSConfigs.ts
@@ -4,20 +4,27 @@ import type { PartialProfileConfig, ShinyConfig } from 'src/types/interfaces'
 
 export default async function findTSConfigs(opts: ShinyConfig): Promise<PartialProfileConfig> {
     let file: string | undefined
+    let found = false
     if (!opts.tsconfigPath) {
         const api = new fdir().withFullPaths().withMaxDepth(1).crawl(opts.root)
         const files = await api.withPromise()
         const length = files.length
         for (let i = 0; i < length; i++) {
             file = files[i]
-            if (file.includes('tsconfig') && file.includes('json')) break
+            if (file.includes('tsconfig') && file.includes('json')) {
+                found = true
+                break
+            }
         }
-    } else file = opts.tsconfigPath
+    } else {
+        file = opts.tsconfigPath
+        found = true
+    }
     return {
         languageOptions: {
             parserOptions: {
                 projectService: {
-                    defaultProject: file ?? 'tsconfig.json'
+                    defaultProject: file && found ? file : 'tsconfig.json'
                 }
             }
         },
