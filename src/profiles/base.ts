ed6378fc814eb15b4ab30bf9591fa3fb35d65739
Torathion <torathion@gmail.com>
2025-01-09 22:23:41 +0100

    change: moved stylistic rules from base to format

--- src/profiles/base.ts
+++ src/profiles/base.ts
@@ -1,8 +1,6 @@
 import type { PartialProfileConfig } from '../types/interfaces'
 import js from '@eslint/js'
 import eslintComments from '@eslint-community/eslint-plugin-eslint-comments'
-import styleJs from '@stylistic/eslint-plugin-js'
-import styleTs from '@stylistic/eslint-plugin-ts'
 import ts from '@typescript-eslint/eslint-plugin'
 import arrayFunc from 'eslint-plugin-array-func'
 import autofix from 'eslint-plugin-autofix'
@@ -12,8 +10,6 @@ import noSecrets from 'eslint-plugin-no-secrets'
 import promise from 'eslint-plugin-promise'
 import regexp from 'eslint-plugin-regexp'
 import unicorn from 'eslint-plugin-unicorn'
-
-import { ALWAYS, FIELD, METHOD, NEVER } from 'src/constants'
 
 export const config: PartialProfileConfig = {
     apply: {
@@ -30,8 +26,6 @@ export const config: PartialProfileConfig = {
         'es-x': es,
         import: importPlugin,
         'no-secrets': noSecrets,
-        styleJs,
-        styleTs,
         ts
     },
     rules: [
@@ -114,56 +108,6 @@ export const config: PartialProfileConfig = {
             'promise/prefer-await-to-then': 1,
             'promise/spec-only': 2,
             'regexp/strict': 2,
-            'styleJs/array-bracket-spacing': 1,
-            'styleJs/computed-property-spacing': 1,
-            'styleJs/dot-location': [1, 'property'],
-            'styleJs/eol-last': 1,
-            'styleJs/generator-star-spacing': [1, 'after'],
-            'styleJs/implicit-arrow-linebreak': 1,
-            'styleJs/multiline-comment-style': 1,
-            'styleJs/new-parens': 1,
-            'styleJs/no-confusing-arrow': 1,
-            'styleJs/no-floating-decimal': 1,
-            'styleJs/no-mixed-spaces-and-tabs': [1, 'smart-tabs'],
-            'styleJs/no-multi-spaces': 1,
-            'styleJs/no-multiple-empty-lines': 1,
-            'styleJs/no-trailing-spaces': 1,
-            'styleJs/no-whitespace-before-property': 1,
-            'styleJs/nonblock-statement-body-position': 1,
-            'styleJs/rest-spread-spacing': [1, NEVER],
-            'styleJs/space-in-parens': 1,
-            'styleJs/space-unary-ops': [1, { nonwords: false, words: true }],
-            'styleJs/spaced-comment': [1, ALWAYS],
-            'styleJs/switch-colon-spacing': 1,
-            'styleJs/template-curly-spacing': 1,
-            'styleJs/template-tag-spacing': 1,
-            'styleJs/wrap-iife': [1, 'inside'],
-            'styleJs/yield-star-spacing': [1, 'after'],
-            'styleTs/brace-style': 1,
-            'styleTs/function-call-spacing': 1,
-            'styleTs/key-spacing': 1,
-            'styleTs/keyword-spacing': 1,
-            'styleTs/lines-between-class-members': [
-                1,
-                {
-                    enforce: [
-                        { blankLine: NEVER, next: FIELD, prev: FIELD },
-                        { blankLine: ALWAYS, next: METHOD, prev: FIELD },
-                        { blankLine: ALWAYS, next: METHOD, prev: METHOD }
-                    ]
-                }
-            ],
-            'styleTs/space-before-blocks': 1,
-            'styleTs/space-before-function-paren': [
-                1,
-                {
-                    anonymous: ALWAYS,
-                    asyncArrow: ALWAYS,
-                    named: NEVER
-                }
-            ],
-            'styleTs/space-infix-ops': 1,
-            'styleTs/type-annotation-spacing': 1,
             'ts/class-methods-use-this': 2,
             'ts/consistent-type-exports': 2,
             'ts/default-param-last': 2,
--- src/profiles/format.ts
+++ src/profiles/format.ts
@@ -1,17 +1,18 @@
 import type { ProfileConfig } from '../types/interfaces'
 
+import styleJs from '@stylistic/eslint-plugin-js'
+import styleTs from '@stylistic/eslint-plugin-ts'
 import perfectionist from 'eslint-plugin-perfectionist'
 
-const partition = {
-    partitionByComment: true,
-    partitionByNewLine: true
-}
-/**
- *   Array of formatting configs
- */
+import { ALWAYS, FIELD, METHOD, NEVER } from 'src/constants'
+
 export const config: Partial<ProfileConfig> = {
     extends: [perfectionist.configs['recommended-natural']],
     name: 'format',
+    plugins: {
+        styleJs,
+        styleTs
+    },
     rules: [
         {
             'perfectionist/sort-classes': [
@@ -55,7 +56,57 @@ export const config: Partial<ProfileConfig> = {
             ],
             'perfectionist/sort-intersection-types': 0,
             'perfectionist/sort-jsx-props': 0,
-            'perfectionist/sort-union-types': 0
+            'perfectionist/sort-union-types': 0,
+            'styleJs/array-bracket-spacing': 1,
+            'styleJs/computed-property-spacing': 1,
+            'styleJs/dot-location': [1, 'property'],
+            'styleJs/eol-last': 1,
+            'styleJs/generator-star-spacing': [1, 'after'],
+            'styleJs/implicit-arrow-linebreak': 1,
+            'styleJs/multiline-comment-style': 1,
+            'styleJs/new-parens': 1,
+            'styleJs/no-confusing-arrow': 1,
+            'styleJs/no-floating-decimal': 1,
+            'styleJs/no-mixed-spaces-and-tabs': [1, 'smart-tabs'],
+            'styleJs/no-multi-spaces': 1,
+            'styleJs/no-multiple-empty-lines': 1,
+            'styleJs/no-trailing-spaces': 1,
+            'styleJs/no-whitespace-before-property': 1,
+            'styleJs/nonblock-statement-body-position': 1,
+            'styleJs/rest-spread-spacing': [1, NEVER],
+            'styleJs/space-in-parens': 1,
+            'styleJs/space-unary-ops': [1, { nonwords: false, words: true }],
+            'styleJs/spaced-comment': [1, ALWAYS],
+            'styleJs/switch-colon-spacing': 1,
+            'styleJs/template-curly-spacing': 1,
+            'styleJs/template-tag-spacing': 1,
+            'styleJs/wrap-iife': [1, 'inside'],
+            'styleJs/yield-star-spacing': [1, 'after'],
+            'styleTs/brace-style': 1,
+            'styleTs/function-call-spacing': 1,
+            'styleTs/key-spacing': 1,
+            'styleTs/keyword-spacing': 1,
+            'styleTs/lines-between-class-members': [
+                1,
+                {
+                    enforce: [
+                        { blankLine: NEVER, next: FIELD, prev: FIELD },
+                        { blankLine: ALWAYS, next: METHOD, prev: FIELD },
+                        { blankLine: ALWAYS, next: METHOD, prev: METHOD }
+                    ]
+                }
+            ],
+            'styleTs/space-before-blocks': 1,
+            'styleTs/space-before-function-paren': [
+                1,
+                {
+                    anonymous: ALWAYS,
+                    asyncArrow: ALWAYS,
+                    named: NEVER
+                }
+            ],
+            'styleTs/space-infix-ops': 1,
+            'styleTs/type-annotation-spacing': 1
         }
     ]
 }
