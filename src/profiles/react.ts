c661ea33e94723f7ac4dcc8997273f690e1f71ac
Torathion <torathion@gmail.com>
2025-06-15 23:07:52 +0200

    fix: rule names in format and react

--- src/profiles/react.ts
+++ src/profiles/react.ts
@@ -1,6 +1,6 @@
 import type { DeepPartial } from 'typestar'
 import react from '@eslint-react/eslint-plugin'
-import styleJsx from '@stylistic/eslint-plugin-jsx'
+import style from '@stylistic/eslint-plugin'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHookForm from 'eslint-plugin-react-hook-form'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -47,7 +47,7 @@ export default function reactConfig(_metadata: ProjectMetadata): DeepPartial<Pro
     plugins: {
       'jsx-a11y': jsxA11y,
       'react-refresh': reactRefresh,
-      styleJsx,
+      style,
       'validate-jsx-nesting': validJsxNesting
     },
     rules: [
@@ -72,21 +72,21 @@ export default function reactConfig(_metadata: ProjectMetadata): DeepPartial<Pro
             ]
           }
         ],
-        'styleJsx/jsx-child-element-spacing': 1,
-        'styleJsx/jsx-closing-bracket-location': [1, 'line-aligned'],
-        'styleJsx/jsx-closing-tag-location': [1, 'tag-aligned'],
-        'styleJsx/jsx-curly-brace-presence': [1, { children: NEVER, propElementValues: ALWAYS, props: NEVER }],
-        'styleJsx/jsx-curly-newline': 1,
-        'styleJsx/jsx-curly-spacing': [1, { attributes: { allowMultiline: false }, children: true, when: NEVER }],
-        'styleJsx/jsx-equals-spacing': 1,
-        'styleJsx/jsx-first-prop-new-line': 1,
-        'styleJsx/jsx-function-call-newline': 1,
-        'styleJsx/jsx-newline': [1, { prevent: true }],
-        'styleJsx/jsx-one-expression-per-line': [1, { allow: 'single-line' }],
-        'styleJsx/jsx-pascal-case': [1, { allowNamespace: true }],
-        'styleJsx/jsx-props-no-multi-spaces': 1,
-        'styleJsx/jsx-self-closing-comp': [1, { component: true, html: true }],
-        'styleJsx/jsx-sort-props': [
+        'style/jsx-child-element-spacing': 1,
+        'style/jsx-closing-bracket-location': [1, 'line-aligned'],
+        'style/jsx-closing-tag-location': [1, 'tag-aligned'],
+        'style/jsx-curly-brace-presence': [1, { children: NEVER, propElementValues: ALWAYS, props: NEVER }],
+        'style/jsx-curly-newline': 1,
+        'style/jsx-curly-spacing': [1, { attributes: { allowMultiline: false }, children: true, when: NEVER }],
+        'style/jsx-equals-spacing': 1,
+        'style/jsx-first-prop-new-line': 1,
+        'style/jsx-function-call-newline': 1,
+        'style/jsx-newline': [1, { prevent: true }],
+        'style/jsx-one-expression-per-line': [1, { allow: 'single-line' }],
+        'style/jsx-pascal-case': [1, { allowNamespace: true }],
+        'style/jsx-props-no-multi-spaces': 1,
+        'style/jsx-self-closing-comp': [1, { component: true, html: true }],
+        'style/jsx-sort-props': [
           1,
           {
             callbacksLast: true,
@@ -96,8 +96,8 @@ export default function reactConfig(_metadata: ProjectMetadata): DeepPartial<Pro
             shorthandFirst: true
           }
         ],
-        'styleJsx/jsx-tag-spacing': 1,
-        'styleJsx/jsx-wrap-multilines': 1,
+        'style/jsx-tag-spacing': 1,
+        'style/jsx-wrap-multilines': 1,
         'validate-jsx-nesting/no-invalid-jsx-nesting': 2
       }
     ],
